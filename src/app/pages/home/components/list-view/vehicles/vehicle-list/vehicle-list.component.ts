1a3e4ef79a26d66c58569fde7c1325037ae3040e
Ritik11131 <ritik11131@gmail.com>
2025-08-01 00:20:30 +0530

    Some Changes

--- src/app/pages/home/components/list-view/vehicles/vehicle-list/vehicle-list.component.ts
+++ src/app/pages/home/components/list-view/vehicles/vehicle-list/vehicle-list.component.ts
@@ -1,44 +1,48 @@
 import { Component, Input } from '@angular/core';
 import { VehicleCardComponent } from './vehicle-card/vehicle-card.component';
-import { VehicleSkeletonCardComponent } from "./vehicle-skeleton-card/vehicle-skeleton-card.component";
+import { VehicleSkeletonCardComponent } from './vehicle-skeleton-card/vehicle-skeleton-card.component';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 @Component({
-  selector: 'app-vehicle-list',
-  imports: [VehicleCardComponent, VehicleSkeletonCardComponent],
-  template: `
+    selector: 'app-vehicle-list',
+    imports: [VehicleCardComponent, VehicleSkeletonCardComponent, ScrollingModule],
+    template: `
         <!-- Cards Wrapper Container -->
-        <div class="max-h-[calc(100vh-180px)] overflow-y-scroll scrollbar-hide mt-4">
-            <div class="flex flex-col gap-4 p-2">
-              @if(isLoading) {
-                  <!-- Show Skeleton Cards when loading -->
-                  @for (i of [1,2,3,4,5,6,7,8,9,10]; track i; let j = $index) {
-                    <app-vehicle-skeleton-card />
-                  }
-              } @else {
-                @for (vehicle of fetchedVehicles; track vehicle?.id; let i = $index) {
-                  <app-vehicle-card [vehicle]="vehicle" [isSelected]="selectedVehicle === vehicle" (cardSelected)="onVehicleSelected($event)" />
+        <div class="max-h-[calc(100vh-280px)] overflow-y-scroll scrollbar-hide mt-4">
+            <div class="flex flex-col gap-4">
+                @if (isLoading) {
+                    <!-- Show Skeleton Cards when loading -->
+                    <div class="px-2">
+                    @for (i of [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; track i; let j = $index) {
+                        <app-vehicle-skeleton-card />
+                      }
+                    </div>
+                } @else {
+                    <cdk-virtual-scroll-viewport itemSize="124" class="scrollbar-hide" style="height: calc(100vh - 280px);">
+                        <div *cdkVirtualFor="let vehicle of fetchedVehicles; let last = last; track: vehicle?.id" [class.mb-4]="!last" class="px-2">
+                            <app-vehicle-card [vehicle]="vehicle" [isSelected]="selectedVehicle === vehicle" (cardSelected)="onVehicleSelected($event)" />
+                        </div>
+                    </cdk-virtual-scroll-viewport>
                 }
-              }
             </div>
         </div>
     `,
-  styles: ``
+    styles: ``
 })
 export class VehicleListComponent {
-  @Input() fetchedVehicles: any[] = [];
-  @Input() isLoading: boolean = false;
+    @Input() fetchedVehicles: any[] = [];
+    @Input() isLoading: boolean = false;
 
-  selectedVehicle: any = null;
+    selectedVehicle: any = null;
 
-  onVehicleSelected(vehicle: any) {
-    this.selectedVehicle = vehicle;
-    console.log(vehicle);
-    
-  }
+    onVehicleSelected(vehicle: any) {
+        this.selectedVehicle = vehicle;
+        console.log(vehicle);
+    }
 
-  ngOnDestroy(): void {
-    // Cleanup logic if needed
-    this.selectedVehicle = null; // Clear the selected geofence reference
-    console.log('VehicleListComponent destroyed and cleaned up');
-  }
+    ngOnDestroy(): void {
+        // Cleanup logic if needed
+        this.selectedVehicle = null; // Clear the selected geofence reference
+        console.log('VehicleListComponent destroyed and cleaned up');
+    }
 }
