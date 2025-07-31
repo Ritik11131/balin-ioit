a920883c1214a81e69b7f388383781dee927f8f1
Ritik11131 <ritik11131@gmail.com>
2025-08-01 00:26:34 +0530

    Some Changes

--- src/app/pages/home/components/list-view/vehicles/vehicle-list/vehicle-list.component.ts
+++ src/app/pages/home/components/list-view/vehicles/vehicle-list/vehicle-list.component.ts
@@ -9,17 +9,17 @@ import { ScrollingModule } from '@angular/cdk/scrolling';
     template: `
         <!-- Cards Wrapper Container -->
         <div class="max-h-[calc(100vh-280px)] overflow-y-scroll scrollbar-hide mt-4">
-            <div class="flex flex-col gap-4">
+            <div class="flex flex-col">
                 @if (isLoading) {
                     <!-- Show Skeleton Cards when loading -->
-                    <div class="px-2">
+                    <div class="flex flex-col gap-4 px-2">
                     @for (i of [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; track i; let j = $index) {
                         <app-vehicle-skeleton-card />
                       }
                     </div>
                 } @else {
                     <cdk-virtual-scroll-viewport itemSize="124" class="scrollbar-hide" style="height: calc(100vh - 280px);">
-                        <div *cdkVirtualFor="let vehicle of fetchedVehicles; let last = last; track: vehicle?.id" [class.mb-4]="!last" class="px-2">
+                        <div *cdkVirtualFor="let vehicle of fetchedVehicles; let last = last; trackBy: trackByVehicleId" [class.mb-4]="!last" class="px-2">
                             <app-vehicle-card [vehicle]="vehicle" [isSelected]="selectedVehicle === vehicle" (cardSelected)="onVehicleSelected($event)" />
                         </div>
                     </cdk-virtual-scroll-viewport>
@@ -34,6 +34,8 @@ export class VehicleListComponent {
     @Input() isLoading: boolean = false;
 
     selectedVehicle: any = null;
+
+    trackByVehicleId = (index: number, vehicle: any) => vehicle?.id ?? index;
 
     onVehicleSelected(vehicle: any) {
         this.selectedVehicle = vehicle;
