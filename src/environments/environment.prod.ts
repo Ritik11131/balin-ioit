a8b2553840ff8f51d13a297904b1c7f06867ba95
Ritik11131 <ritik11131@gmail.com>
2025-08-22 17:55:17 +0530

    Changes in angular.json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
-    production: true,
+    production: false,
     apiUrl: 'https://api.baliniot.in/api', // Replace with your production API URL
     googleMapsApiKey: 'AIzaSyANwFnZ6Y1rv6lh209CahpsqNUnCmCpC58', // Replace with your Google Maps API key
     googleMapStaticApiURl:'https://gm-static-api.vercel.app/api/map',
     storeRefreshInterval: 10 * 60 * 1000,
+    liveStreamingBaseUrl: 'https://live-streaming-6afbf.web.app/index.html'
 }
