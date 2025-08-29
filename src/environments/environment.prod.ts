1aa823dcbe2451e657b8b51ac3b0eeac3fc74560
Ritik11131 <ritik11131@gmail.com>
2025-08-29 11:10:04 +0530

    Some

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
     googleMapsApiKey: 'AIzaSyANwFnZ6Y1rv6lh209CahpsqNUnCmCpC58', // Replace with your Google Maps API key
     googleMapStaticApiURl:'https://gm-static-api.vercel.app/api/map',
     storeRefreshInterval: 10 * 60 * 1000,
-    singleVehiclePollingInterval: 10 * 60 * 1000,
+    singleVehiclePollingInterval: 10 * 1000,
     liveStreamingBaseUrl: 'https://live-streaming-6afbf.web.app/index.html'
 }
